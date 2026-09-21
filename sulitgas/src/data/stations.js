4e07c579cd6a21f6c1048490953ec5f762040e1e
JuztisLeague <vjelciario@gmail.com>
2026-09-21 21:53:43 +0800

    Initial commit: SulitGas Watch basic station list

---
+++ sulitgas/src/data/stations.js
@@ -0,0 +1,7 @@
+export const stations = [
+    {name: "Petron Diversion Road", diesel: 58.45, gasoline: 63.2}, 
+    {name: "Shell Jaro Plaza", diesel: 59.9, gasoline: 64.75},
+    {name: "Caltex Molo", diesel: 57.8, gasoline: 62.95},
+    {name: "Phoenix Tagbak", diesel: 57.35, gasoline: 62.4}
+
+  ];
