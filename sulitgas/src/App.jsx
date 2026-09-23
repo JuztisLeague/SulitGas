5cb05d0c1ebe18fb757ed84b019bd2846ee4fd7d
JuztisLeague <vjelciario@gmail.com>
2026-09-23 14:27:29 +0800

    Refactored App.jsx where the Home Page only recieve the stations instead of creating it

--- sulitgas/src/App.jsx
+++ sulitgas/src/App.jsx
@@ -1,4 +1,6 @@
+import {useState} from "react";
 import {Routes, Route} from "react-router-dom";
+import {stations as initialStations} from "./data/stations";
 import NavBar from "./NavBar";
 import Home from "./pages/Home";
 import Report from "./pages/Report";
@@ -7,13 +9,32 @@ import News from "./pages/News";
 import Ask from "./pages/Ask";
 
 export default function App() {
+  const [stations, setStations] = useState(initialStations)
+  
+  function handleReport(newReport) {
+    setStations((prevStations) => {
+      const existingIndex = prevStations.findIndex(
+        (station) => station.name.toLowerCase() === newReport.name.toLowerCase()
+      );
+
+      if (existingIndex !== -1) {
+        const updated = [...prevStations];
+        updated[existingIndex] = {...updated[existingIndex],
+          [newReport.fuelType]: newReport.price,
+        };
+        return updated;
+      }
+      return prevStations;
+    });
+  }
+  
   return (
     <>
     <NavBar />
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home stations = {stations}/>} />
       <Route path = "/news" element={<News />} />
-      <Route path = "/report" element={<Report />} />
+      <Route path = "/report" element={<Report onReport= {handleReport}/>} />
       <Route path = "/ask" element={<Ask />} />
     </Routes>
     </>
--- sulitgas/src/pages/Home.jsx
+++ sulitgas/src/pages/Home.jsx
@@ -1,23 +1,22 @@
 import {useState} from "react";
-import {stations} from "../data/stations";
 import FuelToggle from "../FuelToggle";
 import StationList from "../StationList"
-import {Link} from "react-router-dom";
 
-export default function Home() {
+export default function Home({stations}) {
 
   const [fuel, setFuel] = useState("diesel");
   const [searchTerm, setSearchTerm] = useState("");
-
+  
   return (
     <div className="app-title">
     <h1>SulitGas Watch</h1>
      <input 
-    type="text"
-    placeholder="Search station or area"
-    value = {searchTerm}
-    onChange ={(e) => setSearchTerm(e.target.value)}
-    className="search-input"></input>
+        type="text"
+        placeholder="Search station or area"
+        value = {searchTerm}
+        onChange ={(e) => setSearchTerm(e.target.value)}
+        className="search-input">
+      </input>
     <FuelToggle fuel={fuel} setFuel={setFuel} />
     <StationList stations = {stations} fuel = {fuel} searchTerm = {searchTerm}/>
     </div>
